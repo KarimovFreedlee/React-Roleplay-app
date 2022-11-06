9c987783e20e77919e91a3946d0238e4f1188026
Vlad Karimov <45467077+KarimovFreedlee@users.noreply.github.com>
2022-11-07 02:23:06 +0300

    characterCreting form

--- src/Components/CharacterCreating/CharacterCreating.js
+++ src/Components/CharacterCreating/CharacterCreating.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import { useAuth } from '../../context/AuthContext';
+import { Form } from "react-bootstrap"
+import "../../ComponentsView/CharacterCreating.css"
 
 export default function CharacterCreating() {
+
+  const { createCharacter } = useAuth()
+  const nameRef = React.useRef()
+
+  const mainClass = "characterCreating"
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newCharacter = {
+      name: nameRef.current.value
+    }
+
+    createCharacter(newCharacter)
+  };
+
   return (
-    <div>CharacterCreating</div>
+    <div className={`${mainClass}`}>
+        <Form onSubmit={handleSubmit} className={`${mainClass}-modal`}>
+        <div className={`${mainClass}-header`} >
+          <input className={`${mainClass}-header-name`} ref={nameRef} type="text"/>
+          <div className={`${mainClass}-header-dropdown`}>
+
+          </div>
+        </div>
+        <div className={`${mainClass}-grid`} >
+        </div>
+          <div className={`${mainClass}-buttons`} >
+            <button className={`${mainClass}-button`} type="submit">create</button>
+            <button className={`${mainClass}-button`}>cancel</button>
+          </div>
+      </Form>
+    </div>
   )
 }
